160c309fc870f0f215b759e3616e40b780507833
naftalima <naftalima@pop-os.localdomain>
2024-01-03 14:47:26 -0300

    2023

--- 2023/01A.py
+++ 2023/01A.py
@@ -1,3 +1,5 @@
+file_name='puzzle-input/'+'calibration-doc.txt'
+
 process_line_digits = lambda lines: (
     (lines[0], lines[0]) if len(lines) == 1 else
     (lines[0], lines[-1]) if len(lines) > 1 else None
@@ -6,7 +8,7 @@ process_line_digits = lambda lines: (
 tupla_to_int = lambda digts: int(digts[0])*10 + int(digts[1])
 
 calibration_digits = []
-with open("calibration-doc.txt") as file:
+with open(file_name) as file:
   for line in file:
     digits_per_line = [i for i in line if i.isdigit()]
     calibration_digits.append(process_line_digits(digits_per_line))
