dff416c79152851e93919b46b9ddfd19a1dcbf65
naftalima <nathaliapvlm@gmail.com>
2024-01-09 12:26:13 -0300

    remove prints

--- 2023/06A.py
+++ 2023/06A.py
@@ -1,24 +1,15 @@
-from termcolor import colored
 import numpy
 
 def count_winnable_races(race_tuple):
     print(race_tuple)
     time, distance = race_tuple
-    print('time:', time)
-    print('distance:', distance)
     count = 0
     for i in range(time):
         hold_bt = i
         velocity = hold_bt
         travel_time = time - hold_bt
         travelled_distance = velocity * travel_time
-        print('hold_bt:', hold_bt)
-        print('velocity:', velocity)
-        print('travel_time:', travel_time)
-        print('travelled_distance:', travelled_distance)
         if travelled_distance > distance:
-            debugger = str(travelled_distance) + ' >= ' + str(distance)
-            print(colored(debugger, 'red'))
             count+=1
     return count
 
